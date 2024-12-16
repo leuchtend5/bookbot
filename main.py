3e953d2c23bc93f007712cf35dcc2ddc3ceb73a7
leuchtend5 <zxcmj1@gmail.com>
2024-12-16 22:34:29 +0700

    feat: add print report function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
     with open("books/frankenstein.txt") as f:
+    report = print_report(chars)
         file_contents = f.read()
         words = file_contents.split()
 
@@ -16,5 +17,22 @@ def count_characters(characters):
             chars_dict[char] += 1
 
     print(chars_dict)
+
+def print_report(words_dict):
+    chars_list = []
+
+    for char in words_dict:
+        if char.isalpha():
+            chars_list.append({"char": char, "count": words_dict[char]})
+
+    chars_list.sort(reverse=True, key=sort_on)
+
+    for chars in chars_list:
+        print(f"The '{chars['char']}' character was found {chars['count']} times")
+
+
+def sort_on(dict):
+    return dict["count"]
+
 
 main()
